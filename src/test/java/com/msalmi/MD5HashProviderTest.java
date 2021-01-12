e3afe5e049002bb6a3d0e14c2faf142e7583d11c
Matheus Salmi <mathsalmi@gmail.com>
2021-01-12 09:58:23 -0300

    Add test of hash with leading 0

--- src/test/java/com/msalmi/MD5HashProviderTest.java
+++ src/test/java/com/msalmi/MD5HashProviderTest.java
@@ -13,7 +13,7 @@ public class MD5HashProviderTest {
 		var encoded = provider.encode("hello world", 0);
 		assertTrue(encoded.equals(expected));
 	}
-	
+
 	@Test
 	public void encodeEmptyString() {
 		final var provider = new MD5HashProvider(MD5HashProviderFactory.ID);
@@ -21,16 +21,24 @@ public class MD5HashProviderTest {
 		var encoded = provider.encode("", 0);
 		assertTrue(encoded.equals(expected));
 	}
-	
+
 	@Test
 	public void ensureIterationParameterIsIgnored() {
 		final var provider = new MD5HashProvider(MD5HashProviderFactory.ID);
 		var expected = "d41d8cd98f00b204e9800998ecf8427e";
 		var encoded = provider.encode("", 0);
 		assertTrue(encoded.equals(expected));
-		
+
 		expected = "d41d8cd98f00b204e9800998ecf8427e";
 		encoded = provider.encode("", 42); // any random number
+		assertTrue(encoded.equals(expected));
+	}
+
+	@Test
+	public void testHashesWithLeadingZeros() {
+		final var provider = new MD5HashProvider(MD5HashProviderFactory.ID);
+		var expected = "0000000018e6137ac2caab16074784a6";
+		var encoded = provider.encode("jk8ssl", 0);
 		assertTrue(encoded.equals(expected));
 	}
 }
